00b3c0706837725baebbd3e33f855cfae90ff4ba
Dipaolo <adipaolo@teksystems.com>
2019-06-20 12:57:10 -0400

    Committing changes to my FirstGitProgram

---
+++ src/gitfiles/FirstGitProgram.java
@@ -0,0 +1,12 @@
+package gitfiles;
+
+public class FirstGitProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//syso + CTRL + space_bar
+		System.out.println("Git Learning");
+
+	}
+
+}
